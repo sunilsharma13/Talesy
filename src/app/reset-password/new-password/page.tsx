8a86facac347664899bbb3301fbb5eb148d5b76a
Macbook <macbook@Sunil-MacBook-Air.local>
2025-06-09 23:57:42 +0530

    Error in reset-password file

--- src/app/reset-password/new-password/page.tsx
+++ src/app/reset-password/new-password/page.tsx
@@ -1,6 +1,13 @@
 // app/reset-password/new-password/page.tsx
 
-import NewPasswordClient from "./NewPasswordClient";
+"use client"; // force full client-side rendering
+
+import dynamic from "next/dynamic";
+
+// Lazy-load client component with suspense
+const NewPasswordClient = dynamic(() => import("./NewPasswordClient"), {
+  ssr: false,
+});
 
 export default function NewPasswordPage() {
   return <NewPasswordClient />;
